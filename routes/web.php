7842a738de9abd756ee547fcd9d64875638f23e1
triaidas <tamas.bozsik99@gmail.com>
2025-10-27 11:32:21 +0100

    welcome screen removed

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PreferencesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
